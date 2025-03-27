a2e60e18ad02e57095a4707965858a804b3a7d1c
olgunbey <olgunsahin0161@hotmail.com>
2025-03-27 23:56:36 +0300

    Add project files.

---
+++ Checkpoint.API/Models/AddBaseUrlRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Checkpoint.API.Models
+{
+    public class AddBaseUrlRequestDto
+    {
+        public string Collectionname { get; set; }
+        public string Jsondata { get; set; }
+    }
+}
---
+++ Checkpoint.API/Program.cs
@@ -0,0 +1,33 @@
+using Checkpoint.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapPost("requsest/addRequest", (AddBaseUrlRequestDto dto) =>
+{
+
+
+});
+
+app.MapControllers();
+
+app.Run();
