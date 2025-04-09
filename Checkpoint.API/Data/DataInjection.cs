abe55aa2bbc9f71ddec0682afcaeeaca943479ee
olgunbey <olgunsahin0161@hotmail.com>
2025-04-09 16:37:14 +0300

    feat: Endpoint added for Project table.

--- Checkpoint.API/Data/DataInjection.cs
+++ Checkpoint.API/Data/DataInjection.cs
@@ -7,7 +7,6 @@ namespace Checkpoint.API.Data
     {
         public static IServiceCollection GetDataServices(this IServiceCollection services, IConfiguration configuration)
         {
-
             services.AddDbContext<CheckpointDbContext>(u => u.UseNpgsql(configuration.GetConnectionString("checkpoint")));
             services.AddScoped<IApplicationDbContext, CheckpointDbContext>();
             return services;
--- Checkpoint.API/Features/Request/Query/GetRequestById.cs
+++ Checkpoint.API/Features/Request/Query/GetRequestById.cs
@@ -13,56 +13,59 @@ namespace Checkpoint.API.Features.Request.Query
 {
     internal static class Processor
     {
-        internal sealed class Query : CustomIRequest<Response>
+        internal sealed class Mediatr
         {
-            public Request Request { get; set; }
-        }
-        internal sealed class Handler : CustomIRequestHandler<Query, Response>
-        {
-            private readonly IApplicationDbContext _applicationDbContext;
-            public Handler(IApplicationDbContext applicationDbContext)
+            internal sealed class Request : CustomIRequest<Dto.Response>
             {
-                _applicationDbContext = applicationDbContext;
+                public Dto.Request RequestDto { get; set; }
             }
-            public async Task<ResponseDto<Response>> Handle(Query request, CancellationToken cancellationToken)
+            internal sealed class Handler : CustomIRequestHandler<Request, Dto.Response>
             {
-                BaseUrl? baseUrl = await _applicationDbContext.BaseUrl.FindAsync(request.Request.Id);
-                if (baseUrl == null)
-                    throw new BaseUrlNotFoundException($"Not found BaseUrl-{request.Request.Id} ");
-
-
-                await _applicationDbContext.BaseUrl.Entry(baseUrl)
-                    .Collection(y => y.Controllers)
-                    .Query()
-                    .Include(y => y.Actions)
-                    .LoadAsync();
-
-                var response = new Response()
+                private readonly IApplicationDbContext _applicationDbContext;
+                public Handler(IApplicationDbContext applicationDbContext)
                 {
-                    BasePath = baseUrl.BasePath,
-                    Controllers = baseUrl.Controllers.Select(y => new Response.Controller()
+                    _applicationDbContext = applicationDbContext;
+                }
+
+                public async Task<ResponseDto<Dto.Response>> Handle(Request request, CancellationToken cancellationToken)
+                {
+                    BaseUrl? baseUrl = await _applicationDbContext.BaseUrl.FindAsync(request.RequestDto.Id);
+                    if (baseUrl == null)
+                        throw new BaseUrlNotFoundException($"Not found BaseUrl-{request.RequestDto.Id} ");
+
+
+                    await _applicationDbContext.BaseUrl.Entry(baseUrl)
+                        .Collection(y => y.Controllers)
+                        .Query()
+                        .Include(y => y.Actions)
+                        .LoadAsync();
+
+                    var response = new Dto.Response()
                     {
-                        ControllerPath = y.ControllerPath,
-                        Id = y.Id,
-                        Actions = y.Actions.Select(y => new Response.Action()
+                        BasePath = baseUrl.BasePath,
+                        Controllers = baseUrl.Controllers.Select(y => new Dto.Response.Controller()
                         {
-                            ActionPath = y.ActionPath,
+                            ControllerPath = y.ControllerPath,
                             Id = y.Id,
-                            Body = y.Body,
-                            Query = y.Query,
-                            Header = y.Header,
-                            RequestType = y.RequestType
+                            Actions = y.Actions.Select(y => new Dto.Response.Action()
+                            {
+                                ActionPath = y.ActionPath,
+                                Id = y.Id,
+                                Body = y.Body,
+                                Query = y.Query,
+                                Header = y.Header,
+                                RequestType = y.RequestType
+                            }).ToList()
+
                         }).ToList()
+                    };
 
-                    }).ToList()
-                };
-
-                return ResponseDto<Response>.Success(response, 200);
-
+                    return ResponseDto<Dto.Response>.Success(response, 200);
+                }
             }
         }
-        internal sealed record Request(int Id);
-        internal sealed class Validator : AbstractValidator<Request>
+
+        internal sealed class Validator : AbstractValidator<Dto.Request>
         {
             public Validator()
             {
@@ -70,27 +73,32 @@ namespace Checkpoint.API.Features.Request.Query
             }
 
         }
-        internal sealed class Response
-        {
-            internal sealed class Controller
-            {
-                public int Id { get; set; }
-                public string ControllerPath { get; set; }
-                public List<Action> Actions { get; set; }
-            }
-            internal sealed class Action
-            {
-                public int Id { get; set; }
-                public string ActionPath { get; set; }
-                public RequestType RequestType { get; set; }
-                public string? Query { get; set; }
-                public string? Header { get; set; }
-                public string? Body { get; set; }
-            }
-            public int Id { get; set; }
-            public string BasePath { get; set; }
-            public List<Controller> Controllers { get; set; }
 
+        internal sealed class Dto
+        {
+            internal sealed record Request(int Id);
+            internal sealed class Response
+            {
+                internal sealed class Controller
+                {
+                    public int Id { get; set; }
+                    public string ControllerPath { get; set; }
+                    public List<Action> Actions { get; set; }
+                }
+                internal sealed class Action
+                {
+                    public int Id { get; set; }
+                    public string ActionPath { get; set; }
+                    public RequestType RequestType { get; set; }
+                    public string? Query { get; set; }
+                    public string? Header { get; set; }
+                    public string? Body { get; set; }
+                }
+                public int Id { get; set; }
+                public string BasePath { get; set; }
+                public List<Controller> Controllers { get; set; }
+
+            }
         }
 
         internal sealed class BaseUrlNotFoundException(string msg) : Exception(msg) { }
@@ -102,7 +110,7 @@ namespace Checkpoint.API.Features.Request.Query
             }
             public async Task<IActionResult> Handler([FromQuery] int id, [FromServices] IMediator mediator, HttpContext httpContext)
             {
-                var data = await mediator.Send(new Query() { Request = new Request(id) });
+                var data = await mediator.Send(new Mediatr.Request() { RequestDto = new Dto.Request(id) });
                 return Handlers(data, httpContext);
             }
         }
