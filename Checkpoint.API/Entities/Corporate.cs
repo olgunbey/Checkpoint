ecf3e09845b08cb9bf21128c410e79f96f19eaad
olgunbey <olgunsahin0161@hotmail.com>
2025-04-09 17:27:12 +0300

    feat: Corporate and Individual table added.

---
+++ Checkpoint.API/Entities/Corporate.cs
@@ -0,0 +1,11 @@
+﻿using Checkpoint.API.Common;
+
+namespace Checkpoint.API.Entities
+{
+    public class Corporate : BaseEntity
+    {
+        public required string Mail { get; set; }
+        public ICollection<Project> Projects { get; set; }
+
+    }
+}
