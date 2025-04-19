d0e110f83dd02257fb4210a109f5f5bfb3296fed
olgunbey <olgunsahin0161@hotmail.com>
2025-04-19 20:18:22 +0300

    feat: The corporateName column in RegisterInbox.

--- Checkpoint.IdentityServer/Data/DatabaseTransactions/CompanyTransaction.cs
+++ Checkpoint.IdentityServer/Data/DatabaseTransactions/CompanyTransaction.cs
@@ -5,9 +5,13 @@ namespace Checkpoint.IdentityServer.Data.DatabaseTransactions
 {
     public class CompanyTransaction(IdentityDbContext identityDbContext)
     {
+        public async Task<Company?> GetCompanyByCompanyKey(string key)
+        {
+            return await identityDbContext.Company.FirstOrDefaultAsync(y => y.Key == key);
+        }
         public async Task<Company?> GetCompanyByCompanyName(string companyName)
         {
-            return await identityDbContext.Company.FirstOrDefaultAsync(y => y.Key == companyName);
+            return await identityDbContext.Company.FirstOrDefaultAsync(y => y.Name == companyName);
         }
     }
 }
