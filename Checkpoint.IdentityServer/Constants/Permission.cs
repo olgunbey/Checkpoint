0f376d5c08ef3efb3ec58113b7e374fe201ce50b
olgunbey <olgunsahin0161@hotmail.com>
2025-04-29 16:45:40 +0300

    feat: permission constant defined

---
+++ Checkpoint.IdentityServer/Constants/Permission.cs
@@ -0,0 +1,15 @@
+﻿namespace Checkpoint.IdentityServer.Constants
+{
+    public class Permission
+    {
+        public const string Admin = "Admin";
+        public const string Ekleme = "Ekleme";
+        public const string Günceleme = "Güncelleme";
+        public const string Onay = "Onay";
+        public const string Rol_Atama = "Rol Atama";
+        public const string Rol_Guncelleme = "Rol Güncelleme";
+        public const string Rol_Silme = "Rol silme";
+        public const string Rol_Ekleme = "Rol ekleme";
+        public const string Okuma = "Okuma";
+    }
+}
