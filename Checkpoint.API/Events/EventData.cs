27cb9ebcfe1f847d5cabbf234d5cab4d474e118a
olgunbey <olgunsahin0161@hotmail.com>
2025-05-01 02:00:25 +0300

    feat: added EventData

---
+++ Checkpoint.API/Events/EventData.cs
@@ -0,0 +1,12 @@
+﻿namespace Checkpoint.API.Events
+{
+    public class EventData
+    {
+        public string ActionId { get; set; }
+        public string Url { get; set; }
+        public bool RequestStatus { get; set; }
+        public int StatusCode { get; set; }
+        public long ResponseTimeMs { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
