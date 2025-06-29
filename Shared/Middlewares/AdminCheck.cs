7739d801bb43f69caa9d1afcc5b7608b0077897c
olgunbey <olgunsahin0161@hotmail.com>
2025-06-29 19:18:53 +0300

    ref: edited query and headers in AdminCheck middleware

--- Shared/Middlewares/AdminCheck.cs
+++ Shared/Middlewares/AdminCheck.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json;
 using Shared.Constants;
 using Shared.Dtos;
@@ -15,13 +16,21 @@ namespace Shared.Middlewares
         public async Task Invoke(HttpContext httpContext)
         {
             var user = httpContext.User;
-            var selectedTeamId = httpContext.Request.Headers["teamId"];
+            StringValues value = StringValues.Empty;
 
-            if (user.Identity is { IsAuthenticated: true } && !string.IsNullOrEmpty(selectedTeamId))
+            httpContext.Request.Headers.TryGetValue("teamId", out value);
+            httpContext.Request.Query.TryGetValue("teamId", out value);
+
+            if (StringValues.IsNullOrEmpty(value))
+            {
+                await _next(httpContext);
+            }
+
+            if (user.Identity is { IsAuthenticated: true })
             {
                 var userTeams = user.Claims.Single(y => y.Type == "teams");
                 var deserializeData = JsonConvert.DeserializeObject<List<CorporateJwtModel>>(userTeams.Value);
-                CorporateJwtModel selectedGetTeam = deserializeData.Single(y => y.TeamId == Int16.Parse(selectedTeamId!));
+                CorporateJwtModel selectedGetTeam = deserializeData.Single(y => y.TeamId == short.Parse(value.ToString()));
                 if (selectedGetTeam.Permissions.Any(y => y == Permission.Admin))
                 {
                     httpContext.Items["AdminByPass"] = true;
