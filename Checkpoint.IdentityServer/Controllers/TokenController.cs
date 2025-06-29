68597c381e4f19bb076e9f466d084ee8cf2c1f2b
olgunbey <olgunsahin0161@hotmail.com>
2025-06-29 21:13:24 +0300

    feat: added routes to ocelot

--- Checkpoint.IdentityServer/Controllers/TokenController.cs
+++ Checkpoint.IdentityServer/Controllers/TokenController.cs
@@ -1,13 +1,12 @@
 ﻿using Checkpoint.IdentityServer.Dtos;
 using Checkpoint.IdentityServer.TokenServices;
 using Microsoft.AspNetCore.Mvc;
-using ServiceStack.Redis;
 
 namespace Checkpoint.IdentityServer.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
-    public class TokenController(IRedisClientAsync redisClientAsync, TokenService tokenService) : BaseController
+    public class TokenController(TokenService tokenService) : BaseController
     {
         [HttpGet]
         public async Task<IActionResult> RemoveRefreshToken([FromQuery] int userId)
@@ -16,7 +15,7 @@ namespace Checkpoint.IdentityServer.Controllers
             return Handlers(responseDto);
         }
         [HttpPost]
-        public async Task<IActionResult> GenerateAccessTokenByRefreshToken([FromBody] GenerateAccessTokenDto generateAccessTokenDto)
+        public async Task<IActionResult> AccessTokenRefreshWithRefreshToken([FromBody] GenerateAccessTokenDto generateAccessTokenDto)
         {
             var responseDto = await tokenService.GenerateAccessTokenAsync(generateAccessTokenDto);
             return Handlers(responseDto);
