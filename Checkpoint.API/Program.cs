78655aa59305599ed5bc5e2323c4e5b8901e4576
olgunbey <olgunsahin0161@hotmail.com>
2025-06-30 14:46:18 +0300

    ref:  edited in the Checkpoint.API

--- Checkpoint.API/Program.cs
+++ Checkpoint.API/Program.cs
@@ -1,16 +1,8 @@
 using Carter;
 using Checkpoint.API.BackgroundJobs;
-using Checkpoint.API.Consumers;
-using Checkpoint.API.Data;
+using Checkpoint.API.DependencyInjections;
 using Hangfire;
-using Hangfire.MemoryStorage;
-using MassTransit;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Scalar.AspNetCore;
-using Shared;
-using Shared.Events;
-using Shared.Hash;
 using Shared.Middlewares;
 
 
@@ -18,10 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddHangfire(y => y.UseMemoryStorage());
-builder.Services.AddHangfireServer();
-builder.Services.AddHttpClient();
-builder.Services.GetDataServices(builder.Configuration);
+builder.Services.AddServices(builder.Configuration);
 
 //using var scope = builder.Services.BuildServiceProvider().CreateScope();
 //var dbContext = scope.ServiceProvider.GetRequiredService<IApplicationDbContext>();
@@ -41,55 +30,8 @@ builder.Services.GetDataServices(builder.Configuration);
 //});
 //await dbContext.SaveChangesAsync(CancellationToken.None);
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidIssuer = "https://localhost:7253",
-            ValidAudience = "https://localhost:5000",
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Hashing.Hash("checkpointsecret"))
-        };
-    });
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        policy.AllowAnyOrigin() 
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
 
-builder.Services.AddOpenApi();
-
-builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssemblyContaining<Program>());
-
-builder.Services.AddMassTransit<IBus>(configure =>
-{
-    configure.AddConsumer<TeamNameReceivedConsumer>();
-    configure.AddRequestClient<GetAllProjectByTeamIdEvent>();
-    configure.UsingRabbitMq((context, configurator) =>
-    {
-        configurator.Host(builder.Configuration.GetSection("AmqpConf")["Host"], config =>
-        {
-            config.Username(builder.Configuration.GetSection("AmqpConf")["Username"]!);
-            config.Password(builder.Configuration.GetSection("AmqpConf")["Password"]!);
-
-        });
-        configurator.ReceiveEndpoint(QueueConfigurations.Identity_Server_TeamNameReceived_Checkpoint_Api, cnf => cnf.ConfigureConsumer<TeamNameReceivedConsumer>(context));
-
-    });
-
-});
-
-builder.Services.AddCarter();
 
 var app = builder.Build();
 
