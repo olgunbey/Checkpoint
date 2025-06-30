526e8aab471580b36b1c0c46c46e59804ef11641
olgunbey <olgunsahin0161@hotmail.com>
2025-06-30 16:16:05 +0300

    ref: name change

--- Checkpoint.API/Dtos/CorporateTokenInformationDto.cs
+++ Checkpoint.API/Dtos/CorporateTokenInformationDto.cs
@@ -5,6 +5,6 @@ namespace Checkpoint.API.Dtos
     public class CorporateTokenInformationDto
     {
         public int UserId { get; set; }
-        public IEnumerable<CorporateJwtModel> CorporateJwtModels { get; set; }
+        public IEnumerable<CorporateJwtTeamModel> CorporateJwtModels { get; set; }
     }
 }
